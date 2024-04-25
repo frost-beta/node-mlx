7677dd423165d33979c74e2d02c2df7d209f7702
Cheng <zcbenz@gmail.com>
2024-04-25 09:36:31 +0900

    Support grep in tests

--- tests/run.ts
+++ tests/run.ts
@@ -1,8 +1,16 @@
 import fs from 'node:fs';
 import path from 'node:path';
 import Mocha from 'mocha';
+import yargs from 'yargs';
+
+const argv = require('yargs')
+  .string('g').alias('g', 'grep')
+  .boolean('i').alias('i', 'invert')
+  .argv;
 
 const mocha = new Mocha();
+if (argv.grep) mocha.grep(argv.grep);
+if (argv.invert) mocha.invert();
 
 for (const f of fs.readdirSync(__dirname)) {
   if (f.endsWith('.spec.ts'))
