1273cd5b8d031cf95089eea0d5350856f0eacd84
Cheng <git@zcbenz.com>
2024-05-04 19:36:36 +0900

    Download metallib file for mac arm64

--- install.js
+++ install.js
@@ -20,21 +20,26 @@ main().catch((error) => {
 });
 
 async function main() {
+  const dir = path.join(__dirname, 'build', 'Release');
+  fs.mkdirSync(dir, {recursive: true});
+
   const os = {darwin: 'mac', win32: 'win'}[process.platform] ?? process.platform;
   const arch = process.arch;
   const version = packageJson.version;
-  const url = `${urlPrefix}/v${version}/mlx-${os}-${arch}.node.gz`;
 
-  const outputDir = path.join(__dirname, 'build', 'Release');
-  fs.mkdirSync(outputDir, {recursive: true});
+  const prefix = `${urlPrefix}/v${version}/mlx-${os}-${arch}`;
+  const tasks = [ download(`${prefix}.node.gz`, path.join(dir, 'mlx.node')) ];
+  if (os == 'mac' && arch == 'arm64')
+    tasks.push(download(`${prefix}.metallib.gz`, path.join(dir, 'mlx.metallib')));
+  await Promise.all(tasks);
+}
 
+async function download(url, filename) {
   const response = await fetch(url);
   if (!response.ok)
-    throw new Error(`HTTP error! Status: ${response.status}`);
+    throw new Error(`Failed to download ${url}, status: ${response.status}`);
 
   const pipeline = util.promisify(stream.pipeline);
   const gunzip = zlib.createGunzip();
-  await pipeline(response.body,
-                 gunzip,
-                 fs.createWriteStream(path.join(outputDir, 'mlx.node')));
+  await pipeline(response.body, gunzip, fs.createWriteStream(filename));
 }
